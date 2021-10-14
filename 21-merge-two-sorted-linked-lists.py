3ad20259726d5f61f19f03e2bd29906f0aa4c129
Nick Mallare <nicholasmallare@gmail.com>
2021-10-13 21:00:09 -0500

    linked list problem

--- 21-merge-two-sorted-linked-lists.py
+++ 21-merge-two-sorted-linked-lists.py
@@ -1,3 +1,4 @@
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,4 +6,22 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
+    
+        currentNode = ListNode()
+        result = currentNode
+        while l1 and l2:
+            if l1.val > l2.val:
+                currentNode.next = l2
+                l2 = l2.next
+            else:
+                currentNode.next = l1
+                l1 = l1.next
+            
+            currentNode = currentNode.next
+
+        #handle edge case of just one list having remaining nodes 
+        else:
+            currentNode.next = l1 or l2
+        return result.next
+
+      
